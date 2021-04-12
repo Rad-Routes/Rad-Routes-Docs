62f6d2172c51fcc661afce67a1e87ffaaf83a4d3
Shyaboi <ian.shane.sears@gmail.com>
2021-04-12 02:12:08 -0700

    added hello world, and prereqs pages

--- .docz/.cache/async-requires.js
+++ .docz/.cache/async-requires.js
@@ -4,9 +4,9 @@ const preferDefault = m => (m && m.default) || m
 exports.components = {
   "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
   "component---readme-md": () => import("./../../../../README.md" /* webpackChunkName: "component---readme-md" */),
-  "component---src-components-alert-mdx": () => import("./../../../../src/components/Alert.mdx" /* webpackChunkName: "component---src-components-alert-mdx" */),
-  "component---src-components-button-mdx": () => import("./../../../../src/components/Button.mdx" /* webpackChunkName: "component---src-components-button-mdx" */),
+  "component---src-hello-world-mdx": () => import("./../../../../src/helloWorld.mdx" /* webpackChunkName: "component---src-hello-world-mdx" */),
   "component---src-index-mdx": () => import("./../../../../src/index.mdx" /* webpackChunkName: "component---src-index-mdx" */),
-  "component---src-pages-404-js": () => import("./../../../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */)
+  "component---src-pages-404-js": () => import("./../../../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-prereq-mdx": () => import("./../../../../src/prereq.mdx" /* webpackChunkName: "component---src-prereq-mdx" */)
 }
 
